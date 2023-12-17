5873930ec3384fe08b1c6b4875a15ea9f29922d9
jp <jpaguinsky@gmail.com>
2023-12-17 14:42:16 -0300

    Refactor Server now only handles connection and game start and closure of connection

--- pkg/Server.java
+++ pkg/Server.java
@@ -1,21 +1,19 @@
 package pkg;
 import java.net.*;
-import java.io.*;
 
 public class Server {
     
     private ServerSocket serverSocket;
     private Socket clientSocket;
-    private static final Integer lives = 5;
+    
 
     public void start(int port) throws Exception{
         System.out.println("Starting Server on port 6666");
         serverSocket = new ServerSocket(port);
-        System.out.println("Waiting for connection");
         clientSocket = serverSocket.accept();
-        HangmanInterface newGame = new HangmanInterface(lives);
-        GameHandler gameHandler = new GameHandler(clientSocket, newGame);
-        gameHandler.run();
+        System.out.println("Connected");
+        GameHandler newGame = new GameHandler(clientSocket);
+        newGame.run();
         clientSocket.close();
         System.out.println("connection closed");
     }   
@@ -23,6 +21,5 @@ public class Server {
     public static void main(String[] args) throws Exception {
         Server server=new Server();
         server.start(6666);
-       
     }
 }
